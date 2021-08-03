fa9393a9dd91f48bebd58587cdb4fb1ba69e9d91
Sophie Kolbe <soko@somewhere.com>
2021-08-03 19:18:46 +0200

    remove vuex from mainjs

--- src/main.js
+++ src/main.js
@@ -1,13 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Vuex from "vuex";
-import { store } from "./store/index.js";
 
 Vue.config.productionTip = false
 
-Vue.use(Vuex);
-
 new Vue({
   render: h => h(App),
-  store: store,
 }).$mount('#app')
